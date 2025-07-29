6484ee5c8b9f242c91ff6b34cc162cfda9c5cbb9
slepming <siskaura06@gmail.com>
2025-07-29 22:00:35 +0300

    Delete comment

--- Transfer.Game/Configuration/PlaylistConfigManager.cs
+++ Transfer.Game/Configuration/PlaylistConfigManager.cs
@@ -10,11 +10,6 @@ internal class PlaylistConfigManager(Storage storage, IDictionary<PlaylistConfig
 
     protected override void InitialiseDefaults()
     {
-        // На самом деле у меня сомнения на счет этого способа хранения состояний плейлистов,
-        // лучше уж было создать JSON файлы там,
-        // но я вероятнее всего буду фиксировать последнее состояние плейлиста(то есть его путь, если он есть),
-        // Если его нет, то путь останется так и нулевым.
-        // Было бы хорошо хранить все плейлисты внутри хранилища приложения, так доступ будет легче, но пожалуй дам выбор пользователю.
         SetDefault(PlaylistConfiguration.Path, string.Empty);
         base.InitialiseDefaults();
     }
